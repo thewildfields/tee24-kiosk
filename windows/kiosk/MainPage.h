3008870718f94c98dd709b0d7492e67378375c73
oleksii-twf <146258454+oleksii-twf@users.noreply.github.com>
2024-11-13 14:39:26 -0800

    Windows Initial commit

---
+++ windows/kiosk/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::kiosk::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::kiosk::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
